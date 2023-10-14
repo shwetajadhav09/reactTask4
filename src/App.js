715a15b306e08da6ac44522299ba86e25312d07c
Shweta <scj99255@gmail.com>
2023-10-14 12:53:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`https://dummyjson.com/users`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.users);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="caption">
+        <h1>Dummy Data</h1></div>
+        <div class="table">
+      <tbody>
+        
+        <tr>
+          <th>S.no</th>
+          <th>Profile Pic</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Gender</th>
+          <th>Email</th>
+          <th>Username</th>
+          <th>Domain</th>
+          <th>IP</th>
+          <th>University</th>
+
+        </tr>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td>{item.id}</td>
+            <td><img src={item.image} alt=" " height={50}></img></td>
+            <td>{item.firstName} </td>
+            <td>{item.lastName}</td>
+            <td>{item.gender}</td>
+            <td>{item.email}</td>
+            <td>{item.username}</td>
+            <td>{item.domain}</td>
+            <td>{item.ip}</td>
+            <td>{item.university}</td>
+          </tr>
+        ))}
+      </tbody>
+    </div>
     </div>
   );
 }
